0e9cfa92c6820526997640b2d601f178571c1894
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2021-03-29 23:05:47 +0900

    Update

--- hello.ts
+++ hello.ts
@@ -1,11 +1,10 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    new Response("Hello world!", {
-      status: 200,
-      headers: {
-        server: "denosr",
-        "content-type": "text/plain",
-      },
-    })
-  );
-});
+addEventListener('fetch', (event) => {
+  event.respondWith(new Response(JSON.stringify({
+    url: event.request.url,
+    method: event.request.method,
+    hello: 'Hello, Deno Deploy!',
+    headers: Object.fromEntries(event.request.headers.entries())
+  }, null, 2), {
+    headers: { "content-type": "application/json; charset=UTF-8" },
+  }))
+})
